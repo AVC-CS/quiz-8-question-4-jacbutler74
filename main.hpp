491e2ba90b64d8804c3cf0f38753f5d03c95b22d
jacbutler74 <jacbutler74@gmail.com>
2024-02-25 20:30:21 -0800

    Finished main.hpp

--- main.hpp
+++ main.hpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <cstring>
 using namespace std;
 
 const int MAXNUM = 10;
 const int MAXLEN = 100;
 
-int splitline(string, char, char[][MAXLEN]);
+int splitline(string str, char delim, char arr[MAXNUM][MAXLEN]);
 
-/*******************************
- * Code your program here
- *******************************/
+int splitline(string str, char delim, char arr[MAXNUM][MAXLEN]) {
+    stringstream tstr;
+    tstr << str;
+    string s;
+    int cnt = 0;
+
+    for (int i = 0; i < MAXNUM; ++i) {
+        if (getline(tstr, s, delim)) {
+            strcpy(arr[i], s.c_str());
+            ++cnt;
+        }
+        else {
+            break;
+        }
+    }
+
+    return cnt;
+}
